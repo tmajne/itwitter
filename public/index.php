a8aa1b1df4c519f568088390dc1bd72f5230c28b
TT <ttomczyk@gmail.com>
2020-07-28 07:56:51 +0200

    'WIP'

--- public/index.php
+++ public/index.php
@@ -3,4 +3,7 @@
 <body>
     <div id="twitter-widget"></div>
     <script src="/js/twitterWidget.js"></script>
+    <script>
+        const twitterClient = new TwitterClient('localhost:8880')
+    </script>
 </body>
--- public/js/twitterWidget.js
+++ public/js/twitterWidget.js
@@ -1,9 +1,16 @@
 
-let conn = new WebSocket('ws://localhost:8880');
-conn.onopen = function(e) {
-    console.log("Connection established!");
-};
+function TwitterClient(host) {
 
-conn.onmessage = function(e) {
-    console.log(e.data);
-};
+    const conn = new WebSocket('ws://'+ host);
+
+    conn.onopen = function(e) {
+        console.log("Connection established!");
+    };
+
+    conn.onmessage = function(e) {
+        console.log(e.data);
+    };
+
+    return {
+    }
+}
